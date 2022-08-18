331b5150f198c838aa790c28378b291a95ca0476
Junior Sousa <jrsousadev@gmail.com>
2022-08-17 21:53:35 -0300

    (main): create characterAPI

---
+++ src/services/characterApi/character.ts
@@ -0,0 +1,66 @@
+import {
+  CREATE_CHARACTER,
+  DELETE_CHARACTER,
+  GET_ALL_CHARACTERS,
+} from "./characterRoutesApi";
+import { api as ApiClient } from "../apiClient";
+import { GetServerSidePropsContext } from "next";
+import { GetCorrectApi } from "../../utils/GetCorrectApi";
+
+interface ICreateCharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  episode: string[];
+  url: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+}
+
+export const createCharacter = async (data: ICreateCharacter) => {
+  try {
+    const response = await ApiClient.post(CREATE_CHARACTER, data);
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+interface IGetAllCharacter {
+  ctx?: GetServerSidePropsContext | undefined;
+}
+
+export const getAllCharacter = async ({ ctx }: IGetAllCharacter) => {
+  const api = GetCorrectApi(ctx);
+
+  try {
+    const response = await api.get(GET_ALL_CHARACTERS);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+interface IDeleteCharacter {
+  id: number;
+}
+
+export const deleteCharacter = async ({ id }: IDeleteCharacter) => {
+  try {
+    const response = await ApiClient.delete(DELETE_CHARACTER(id));
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
---
+++ src/services/characterApi/characterRoutesApi.ts
@@ -0,0 +1,3 @@
+export const CREATE_CHARACTER = "character/";
+export const GET_ALL_CHARACTERS = "character/";
+export const DELETE_CHARACTER = (id: number) => `character/${id}`;
